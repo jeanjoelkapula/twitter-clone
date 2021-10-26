9ed5860c5bb2df3aa8154f7334bde61c019cb461
jeanjoelkapula <jeanjoelkapula@gmail.com>
2021-10-26 14:27:50 +0200

    Add base model structure

--- network/models.py
+++ network/models.py
@@ -3,4 +3,13 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.ManyToManyField('self', symmetrical = False, related_name = 'followees')
+
+class Post(models.Model):
+    user = models.ForeignKey(User, null=False, on_delete = models.CASCADE)
+    date_created = models.DateTimeField(auto_now=True)
+
+class PostLike(models.Model):
+    user = models.ForeignKey(User, null=False, on_delete = models.CASCADE)
+    Post = models.ForeignKey(Post, null=False, on_delete = models.CASCADE)
+    is_dislike = models.BooleanField(default=False)
