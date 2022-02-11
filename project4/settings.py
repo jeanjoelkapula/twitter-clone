9b47e1903c5a54cb682739c6a8ebbf4d809482ae
Jean Joel Kapula <jeanjoelkapula@gmail.com>
2022-02-11 22:16:46 +0200

    Update settings

--- project4/settings.py
+++ project4/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django
 import django_heroku
 
+
+django.setup()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
